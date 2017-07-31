486caf8746cd027bc66e579314756ecb1b509e02
Shahan <shahan@iii.global>
2017-07-31 16:12:00 -0700

    testing

---
+++ index.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+// require all the components
+const buttons = {
+    toggleButton: require('./src/button/ToggleButton.vue')
+}
+
+const firebaseAdminPanel = require('./src/firebaseAdminPanel/index.vue')
+
+
+module.exports =  {
+    buttons,
+    firebaseAdminPanel
+}
