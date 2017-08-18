3050cf25acf92a81027feaa74e67d1fb15f84a85
Shahan <shahan@iii.global>
2017-08-18 16:31:11 -0700

    Fixed form empty checking

--- src/input/autoform/index.vue
+++ src/input/autoform/index.vue
@@ -141,13 +141,18 @@ export default {
                 // console.log(name, 'is required:', required);
                 if(required) {
                     // console.log(name, required, component.isEmpty());
-                    // console.log(name, typeof component.isEmpty === 'function', component.isEmpty());
-                    if(typeof component.isEmpty === 'function' && component.isEmpty()){
+                    // console.warn(name, typeof component.isEmpty === 'function', component.isEmpty());
+                    if(typeof component.isEmpty === 'function'){
                         // console.log(`${name} is empty`)
                         // console.log(name, required, component.isEmpty(), "RETURNING FALSE");
-                        return false;
+                        if(component.isEmpty())
+                            return false;
                     }
-                    return component.getValue();
+                    else {
+                        // otherwise, the best we can do is hope for a truthy value. we dont know what constitutes as the component being empty.
+                        // all components currently have isEmpty
+                        return component.getValue();
+                    }
                 }
 
                 return true;
