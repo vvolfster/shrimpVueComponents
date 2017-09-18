041fc0ac7625364b1a6a24490d6748455379e51c
Shahan <shahan@iii.global>
2017-09-18 13:19:14 -0700

    More lenient in generic promise resolver

--- src/misc/functions/index.js
+++ src/misc/functions/index.js
@@ -8,6 +8,9 @@ export default {
             if(returnVal instanceof Promise){
                 return returnVal.then(resolve).catch(reject);
             }
+            if(returnVal === undefined) // undefined is ok because most functions are going to return this. But other falsy values arent!!
+                return resolve();
+
             return returnVal ? resolve() : reject();
         })
     }
