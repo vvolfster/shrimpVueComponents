523bb94863707d9dbea728c9cea172b29572c1a6
Shahan <shahan@iii.global>
2018-02-16 14:31:32 -0800

    Added more types to readOnly

--- src/input/readOnly/index.vue
+++ src/input/readOnly/index.vue
@@ -24,7 +24,7 @@ export default {
             default: null
         },
         value: {
-            type: [String, Boolean],
+            type: [String, Boolean, Number, null, undefined],
             default: ''
         },
         placeholder: {
