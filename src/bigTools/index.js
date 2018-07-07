79485f2689606ec41ef170208a0f0c7ab127070b
Shahan Kazi <shahan@zabaat.com>
2018-07-07 15:14:42 -0700

    exporse imageStorage

--- src/bigTools/index.js
+++ src/bigTools/index.js
@@ -1,7 +1,9 @@
 import firebaseAdminPanel from './firebaseAdminPanel'
 import firebaseAdminPanelFb from './firebaseAdminPanel/fbase'
+import imageStorage from "./firebaseAdminPanel/components/imageStorage"
 
 export default {
     firebaseAdminPanel,
-    firebaseAdminPanelFb
+    firebaseAdminPanelFb,
+    imageStorage
 }
